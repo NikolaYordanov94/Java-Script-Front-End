0f9296f7a9ffa8d02cfdbd3f7a06041db7d1e0ba
NikolaYordanov <nikola_bg94@abv.bg>
2023-05-29 01:26:46 +0300

    Initial Commit

---
+++ JS Syntax Fundamentals/Lab/LargestNumber_06.js
@@ -0,0 +1,15 @@
+function largestNum(num1, num2, num3){
+    let result = 0;
+
+    if(num1 > num2 && num1 > num3){
+        result = num1;
+    }else if(num2 > num1 && num2 > num3){
+        result = num2;
+    }else{
+        result = num3;
+    }
+
+    console.log(`The largest number is ${result}.`)
+}
+
+largestNum(5, -3, 16);
