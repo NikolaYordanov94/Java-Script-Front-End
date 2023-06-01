92350ccb3ef67e73843ca86419dcf7c72f6f04b8
NikolaYordanov <nikola_bg94@abv.bg>
2023-06-02 00:25:46 +0300

    Initial Commit

---
+++ JS Syntax Fundamentals/Exercises/Fruit_09.js
@@ -0,0 +1,10 @@
+function fruit(fruitType, weightInGrams, pricePerKg) {
+
+    let weightInKg = weightInGrams / 1000;
+    let totalPrice = weightInKg * pricePerKg;
+
+    console.log(`I need $${totalPrice.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruitType}.`)
+}
+
+fruit('orange', 2500, 1.80);
+fruit('apple', 1563, 2.35);
