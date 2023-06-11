f08446cb70f301db78bc15df1359cdf677c9c71d
NikolaYordanov <nikola_bg94@abv.bg>
2023-06-11 11:03:17 +0300

    Initial Commit

---
+++ Functions and Statements/Exercises/AddAndSubtract_02.js
@@ -0,0 +1,20 @@
+function addAndSubtract(num1, num2, num3) {
+
+    let result = sum(num1, num2) - num3;
+    console.log(result);
+
+
+    function sum(num1, num2) {
+        return num1 + num2;
+    }
+}
+
+addAndSubtract(23,
+    6,
+    10
+);
+
+addAndSubtract(1,
+    17,
+    30
+);
