81e14a921cee7a9b1b7dd50b7ad3ff61f1af988a
NikolaYordanov <nikola_bg94@abv.bg>
2023-06-15 00:08:05 +0300

    Initial Commit

---
+++ Objects and Classes/Lab/CityTaxes_03.js
@@ -0,0 +1,29 @@
+function cityTaxes(name, population, treasury) {
+    let city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            this.population *= (1 + (percentage / 100));
+        },
+        applyRecession(percentage) {
+            this.treasury *= (1 - (percentage / 100));
+        }
+    };
+
+    return city;
+}
+
+const city =
+    cityTaxes('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
+
