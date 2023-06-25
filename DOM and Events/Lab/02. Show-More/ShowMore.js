b323a2273f39a94af6e62e693a1fad82b5f511f1
NikolaYordanov <nikola_bg94@abv.bg>
2023-06-26 00:38:34 +0300

    Initial Commit

---
+++ DOM and Events/Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const anchorLink = document.getElementById("more");
+    const text = document.getElementById("text");
+
+    anchorLink.style.display = "none";
+    text.style.display = "inline";
+}
